c82d39235bbf388beaaac0fa03cb352dd350dc2a
KaoruNishikawa <KaoruNishikawa@users.noreply.github.com>
2022-06-18 14:14:24 +0000

    deploy: 54a510ca20f18eaf70a17dccc88b142c59f50ccf

---
+++ src/js/event.js
@@ -0,0 +1,56 @@
+"use strict"
+
+import * as data from "./data.js"
+import * as graph from "./graph.js"
+
+
+async function dropHandler(event) {
+    event.preventDefault()  // Default is to open the file in current window.
+
+    d3.select(this)
+        .classed("drag-over", false)
+
+    console.info("File(s) dropped")
+
+    const files = []
+    if (event.dataTransfer.items) {
+        for (let data of event.dataTransfer.items) {
+            if (data.kind === "file") {
+                const file = data.getAsFile()
+                console.info(`Reading ${file.name}`)
+                files.push(file)
+            }
+        }
+    } else {
+        for (let file of event.dataTransfer.files) {
+            console.info(`Reading ${file.name}`)
+            files.push(file)
+        }
+    }
+    console.info(`Read ${files.length} file${files.length === 1 ? "" : "s"}`)
+
+    const networkData = await data.readTOML(files)
+    new graph.NetworkGraph(networkData).focusOn()
+}
+
+
+function dragOverHandler(event) {
+    event.preventDefault()
+
+    d3.select(this)
+        .classed("drag-over", true)
+        .on("dragend", ev => d3.select(this).classed("drag-over", false))
+        .on("dragleave", ev => d3.select(this).classed("drag-over", false))
+}
+
+
+async function clickHandler(event) {
+    event.preventDefault()
+
+    const url = d3.select("#data-file-url").property("value")
+    const networkData = await data.readTOML(url)
+    new graph.NetworkGraph(networkData).focusOn()
+}
+
+
+export { dropHandler, dragOverHandler, clickHandler }
